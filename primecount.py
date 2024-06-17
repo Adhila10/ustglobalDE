fc180c44be0c63c5ac1ef5409a9c6adac598569d
Adhila10 <adhilaaboobakar10@gmail.com>
2024-06-17 12:54:28 +0530

    initial commit

---
+++ primecount.py
@@ -0,0 +1,28 @@
+
+
+arr = [2,3,2,3,5,6]
+
+prime=[]
+new=[]
+for num in range(len(arr)):
+    for i in range(2,num):
+        if num%i == 0:
+            print("hii")
+            break
+        else:
+            prime.append(num)
+
+print(prime)
+
+for i in prime:
+    if prime.count(i) > 1:
+        new.append(i)
+
+print(new)
+
+
+
+
+
+
+
